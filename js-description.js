599f56472eb08e07d4c74bd997e167baa18eef6c
Kenne Tsasse Francis <harfleur20@gmail.com>
2025-08-08 16:09:22 +0100

    mise à jour-6

---
+++ js-description.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const accordionItems = document.querySelectorAll('.accordion-item');
+
+    accordionItems.forEach(item => {
+        const header = item.querySelector('.accordion-header');
+        header.addEventListener('click', () => {
+            // Ferme tous les autres accordéons
+            accordionItems.forEach(otherItem => {
+                if (otherItem !== item) {
+                    otherItem.classList.remove('active');
+                }
+            });
+
+            // Ouvre ou ferme l'accordéon cliqué
+            item.classList.toggle('active');
+        });
+    });
+});
